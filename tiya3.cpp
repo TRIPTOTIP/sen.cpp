d96c03810bd3c930b8baf98a003eb6729465b482
TRIPTOTIP <tiyashasenhs@gmail.com>
2024-07-20 17:21:47 +0530

    first commit

---
+++ tiya3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//this program was created by tiya
+// this 
+// is 
+// my namw
+int main()
+{
+    int sum=6;
+cout<<"hello world"<<sum;
+}
---
+++ tiya8.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=34;
+    char c='c';
+   cout<<"the value of a was" <<a<<endl;
+    a=9;
+    c='c';
+    //constants
+    const  a =34;
+    cout<<"the value of a is" <<a<<endl;
+   return 0;
+}
